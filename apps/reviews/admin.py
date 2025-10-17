d9a9b5d66f1901a8925a9cd55df1486122d503e9
ismat-dev <bek919595email@gmail.com>
2025-10-17 14:58:22 +0500

    Yangilanish: <nima o'zgartirdingiz?>

--- apps/reviews/admin.py
+++ apps/reviews/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Review
+from unfold.admin import ModelAdmin
 
 @admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display=['comment_for_room', 'comment', 'user', 'created_at']
+class ReviewAdmin(ModelAdmin):
+    list_display=['comment_for_room', 'comment', 'user', 'created_at', 'rating']
--- apps/reviews/models.py
+++ apps/reviews/models.py
@@ -6,5 +6,13 @@ class Review(models.Model):
     comment_for_room = models.ForeignKey(Room, on_delete=models.CASCADE)
     comment = models.TextField()
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    RATING_CHOICES = [
+        (1, '⭐'),
+        (2, '⭐⭐'),
+        (3, '⭐⭐⭐'),
+        (4, '⭐⭐⭐⭐'),
+        (5, '⭐⭐⭐⭐⭐'),
+    ]
+    rating = models.IntegerField(choices=RATING_CHOICES, default=1)
     created_at = models.DateField(auto_now=True)
     
